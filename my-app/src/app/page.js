90bfad96f880d828a046f19f3e7dc24410a6077d
abbazsr795 <abbazsr795@gmail.com>
2024-02-22 20:49:17 +0530

    made a nice home page

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,9 +1,42 @@
+"use client"
+
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { toaster } from "evergreen-ui";
 
 export default function Home() {
+
+  const {push} = useRouter()
+
   return (
-    <div>
-      
+    <div className="p-5 m-2">
+      <p className="font-bold text-5xl text-center">A framework where you copy code built using other Js libraries instead of installing libraries</p>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div className="flex flex-row justify-center">
+        <button onClick={() => push("/buttons")} className="border border-1 border-slate-600 hover:border-slate-400 rounded px-3 py-2 w-full text-md text-slate-600 hover:text-slate-400">Buttons</button>
+      </div>
+      <br></br>
+      <div className="flex flex-row justify-center">
+        <button onClick={() => push("/cards")} className="border border-1 border-slate-600 hover:border-slate-400 rounded px-3 py-2 w-full text-md text-slate-600 hover:text-slate-400">Cards</button>
+      </div>
+      <br></br>
+      <div className="flex flex-row justify-center">
+        <button onClick={() => toaster.notify("Scroll down!")} className="border border-1 border-slate-600 hover:border-slate-400 rounded px-3 py-2 w-full text-md text-slate-600 hover:text-slate-400">Footer</button>
+      </div>
+      <br></br>
+      <div className="flex flex-row justify-center">
+        <button onClick={() => toaster.notify("Look up!")} className="border border-1 border-slate-600 hover:border-slate-400 rounded px-3 py-2 w-full text-md text-slate-600 hover:text-slate-400">Header</button>
+      </div>
+      <br></br>
+      <div className="flex flex-row justify-center">
+        <button onClick={() => push("/signin")} className="border border-1 border-slate-600 hover:border-slate-400 rounded px-3 py-2 w-full text-md text-slate-600 hover:text-slate-400">SignIn</button>
+      </div>
+      <br></br>
+      <div className="flex flex-row justify-center">
+        <button onClick={() => push("/signup")} className="border border-1 border-slate-600 hover:border-slate-400 rounded px-3 py-2 w-full text-md text-slate-600 hover:text-slate-400">SignUp</button>
+      </div>
     </div>
   );
 }
--- my-app/src/app/{components}/header.js
+++ my-app/src/app/{components}/header.js
@@ -3,14 +3,18 @@
 import Image from "next/image"
 import { Popover, Position, Menu, Button, toaster } from "evergreen-ui"
 import ExpandIcon from "../{icons}/menu-icon-1.png"
+import { useRouter } from "next/navigation"
 
 //The Header utilises both this page and the layout page for it positioning
 
 export default function Header (){
+
+    const {push} = useRouter()
+
     return(
         <div className="fixed left-0 right-0 top-0 w-screen bg-white">
             <div className="flex flex-row justify-between md:justify-around items-center py-3 px-5">
-               <p className="font-bold text-xl">AbbazsJS</p>
+               <button onClick={()  => push("/")} className="font-bold text-xl">AbbazsJS</button>
                <div className="md:hidden">
                     {/* Requires Popover, Position, Menu, Button, toaster from Evergreen-UI */}
                     <Popover
@@ -18,16 +22,14 @@ export default function Header (){
                     content={
                         <Menu>
                         <Menu.Group>
-                            <Menu.Item onSelect={() => toaster.notify('Share')}>Share...</Menu.Item>
-                            <Menu.Item onSelect={() => toaster.notify('Move')}>Move...</Menu.Item>
-                            <Menu.Item onSelect={() => toaster.notify('Rename')} secondaryText="⌘R">
-                            Rename...
-                            </Menu.Item>
+                            <Menu.Item onClick={()  => push("https://github.com/abbazsr795/reusable-components")}>Docs</Menu.Item>
+                            <Menu.Item onClick={()  => push("https://github.com/abbazsr795")}>GitHub</Menu.Item>
+                            <Menu.Item onClick={() => push("https://www.linkedin.com/in/abbazs-rahman-968b65260/")}>LinkedIn</Menu.Item>
                         </Menu.Group>
                         <Menu.Divider />
                         <Menu.Group>
-                            <Menu.Item onSelect={() => toaster.danger('Delete')} intent="danger">
-                            Delete...
+                            <Menu.Item onSelect={() => toaster.notify('Divider Button')} intent="danger">
+                            Button
                             </Menu.Item>
                         </Menu.Group>
                         </Menu>
@@ -41,9 +43,9 @@ export default function Header (){
                <div className="md:block hidden">
                     <div className="flex flex-row gap-5">
                         {/* Uses global.css for removing default button styling */}
-                        <button className="text-slate-500 hover:text-black">Docs</button>
-                        <button className="text-slate-500 hover:text-black">Github</button>
-                        <button className="text-slate-500 hover:text-black">LinkedIn</button>
+                        <button onClick={()  => push("https://github.com/abbazsr795/reusable-components")} className="text-slate-500 hover:text-black">Docs</button>
+                        <button onClick={()  => push("https://github.com/abbazsr795")} className="text-slate-500 hover:text-black">GitHub</button>
+                        <button onClick={() => push("https://www.linkedin.com/in/abbazs-rahman-968b65260/")} className="text-slate-500 hover:text-black">LinkedIn</button>
                     </div>
                </div>
             </div>
