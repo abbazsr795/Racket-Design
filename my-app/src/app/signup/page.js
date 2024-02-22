abc784f7648b890500143d60fb7a8f9c0bc43b46
abbazsr795 <abbazsr795@gmail.com>
2024-02-22 13:24:13 +0530

    sign in and sigup components complete

---
+++ my-app/src/app/signup/page.js
@@ -0,0 +1,31 @@
+"use client"
+
+import React from "react";
+import { Checkbox } from "evergreen-ui";
+
+export default function SignUp(){
+    return(
+        <div className="border bprder-1 p-10 m-2 rounded w-2/6 min-w-fit">
+            <p className="flex flex-row justify-center text-2xl">Create Account</p>
+            <br></br>
+            <p className="text-md">Email</p>
+            {/* Uses global.css for removing blue default outline when focus */}
+            <input maxLength={100} className="border-solid border-slate-300 rounded border p-1 w-full"></input>
+            <br></br>
+            <br></br>
+            <p className="text-md">Password</p>
+            {/* Uses global.css for removing blue default outline when focus */}
+            <input maxLength={100} type={"password"} className="border-solid border-slate-300 rounded border p-1 w-full"></input>
+            {/* Requires Evergreen-UI */}
+            <Checkbox
+            label="Show password"
+            />
+            <br></br>
+            <a href="/signin" className="text-xs flex flex-row justify-center hover:underline">Already have an account? Log in!</a>
+            <br></br>
+            <div className="flex flex-row justify-center">
+                <button className="border border-1 border-slate-300 hover:border-slate-400 rounded px-3 py-2 w-fit text-md">Continue</button>
+            </div>
+        </div>
+    )
+}
